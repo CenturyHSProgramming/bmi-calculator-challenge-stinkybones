4e0c1736e87a1c5eccf020ebd2ed48508bcd5489
hundredvisionsguy <cwinikka@gmail.com>
2017-04-05 10:12:04 -0700

    fixing TestBMICalculator & README

--- README.md
+++ README.md
@@ -1,47 +1,27 @@
-# answerCall
-A Python 3 If-Structure challenge with unit tests. Your job: program Python to automatically answer calls or not based on 
-
-**Rules:**
-----------
-* if you don't know the caller and it's a different area code, don't answer
-* if you don't know the caller and it's the same area code, answer
-* if it's a relative or friend, answer
-* if it's a telemarketer, don't answer
-* if it's after 10pm or before 7am, don't answer (no matter who it is)
+# BMICalculator.py
+A Python 3 Math challenge using unit tests. Your job is to write a function in BMICalculator.py (call it **calculateBMI()** that calculates a user's BMI (according to http://flightphysical.com/medical-exam/weight)
 
 **Inputs:**
 ----------
-* **answerCall()** receives three inputs (string boolean string): caller_code, sameAreaCode, and cur_time
-  * **caller_codes**: are as follows:
-    * "U" = unknown
-    * "F" = friend
-    * "R" = relative
-    * "T" = telemarketer
-  * **sameAreaCode** is a Boolean
-    * True = same area code as yours
-    * False = different area code as yours
-  * **cur_time** stands for current time and is in the 24-hour (military time) format
-    * "09:15" = 9:15AM
-    * "12:00" = 12:00PM
-    * "14:00" = 2:00PM
-    * "22:45" = 10:45PM
-    * "00:30" = 12:30AM
-    
+* **calculateBMI()** receives two inputs (both integers):
+  * receives total inches as an integer
+  * receives weight as an integer
+
 **Outputs:**
 ------------
-* **answerCall()** returns 1 output (a boolean): 
-    * True = answer the call
-    * False = don't answer the call
-    
-**Examples:** 
+* **calculateBMI()** returns 1 output (an integer):
+    * this is the BMI value
+
+**Examples:**
 inputs => output/s
 --------------------------------
-* "U" True "09:00" => True
-* "U" False "14:00" => False
-* "U" True "23:50" => False
-* "T" True "10:40" => False
-* "T" False "23:00" => False
-* "F" False "10:00' => True
-* "F" False "23:00" => False
-* "R" True "9:00" => True
-* "R" False "4:00" => False
+* 62 120 => 22
+* 60 123 => 24
+* 76 197 => 24
+* 64 157 => 27
+* 59 143 => 29
+* 61 222 => 42
+* 58 91 => 19
+* 70 146 => 21
+* 70 271 => 39
+* 58 258 => 54
--- TestBMICalculator.py
+++ TestBMICalculator.py
@@ -10,96 +10,96 @@
 
 # Import Statements
 import unittest
-import CalculatorFunctions
+import BMICalculator
 
 class KnownValues(unittest.TestCase):
     # Formula for unittest method is:
     # test_functionName_testDescription
-    
+
     def test_calculateBMI_forNormalBoundary(self):
         # Capture the results of the function
-        result = CalculatorFunctions.calculateBMI(62, 120)
+        result = BMICalculator.calculateBMI(62, 120)
         # Check for expected output
         self.assertEqual(22, result)
 
     def test_calculateBMI_forFirstNormalValueLowerHeight(self):
-        result = CalculatorFunctions.calculateBMI(60, 123)
+        result = BMICalculator.calculateBMI(60, 123)
         expected = 24
         self.assertEqual(expected, result)
 
     # Add minimum of 5 more unittests
     def test_calculateBMI_forTallNormalValueLowerHeight(self):
-        result = CalculatorFunctions.calculateBMI(76, 197)
+        result = BMICalculator.calculateBMI(76, 197)
         expected = 24
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for1stOverweightValue(self):
-        result = CalculatorFunctions.calculateBMI(64, 157)
+        result = BMICalculator.calculateBMI(64, 157)
         expected = 27
         self.assertEqual(expected, result)
-        
+
     def test_calculateBMI_for2ndOverweightValue(self):
-        result = CalculatorFunctions.calculateBMI(59, 143)
+        result = BMICalculator.calculateBMI(59, 143)
         expected = 29
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for1stObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(67, 204)
+        result = BMICalculator.calculateBMI(67, 204)
         expected = 32
         self.assertEqual(expected, result)
-        
+
     def test_calculateBMI_for2ndObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(69, 230)
+        result = BMICalculator.calculateBMI(69, 230)
         expected = 34
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for3rdObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(61, 185)
+        result = BMICalculator.calculateBMI(61, 185)
         expected = 35
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for4thObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(69, 250)
+        result = BMICalculator.calculateBMI(69, 250)
         expected = 37
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for5thObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(75, 287)
+        result = BMICalculator.calculateBMI(75, 287)
         expected = 36
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for6thObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(70, 271)
+        result = BMICalculator.calculateBMI(70, 271)
         expected = 39
         self.assertEqual(expected, result)
 
-    def test_calculateBMI_for1stExtremeObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(61, 222)
-        expected = 42
+    def test_calculateBMI_forLowNormalValue(self):
+        result = BMICalculator.calculateBMI(58, 91)
+        expected = 19
         self.assertEqual(expected, result)
-        
+
     def test_calculateBMI_for2ndExtremeObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(66, 260)
+        result = BMICalculator.calculateBMI(66, 260)
         expected = 42
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for3rdExtremeObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(58, 258)
+        result = BMICalculator.calculateBMI(58, 258)
         expected = 54
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for4thExtremeObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(74, 420)
+        result = BMICalculator.calculateBMI(74, 420)
         expected = 54
         self.assertEqual(expected, result)
 
     def test_calculateBMI_for5thExtremeObeseValue(self):
-        result = CalculatorFunctions.calculateBMI(76, 443)
+        result = BMICalculator.calculateBMI(76, 443)
         expected = 54
         self.assertEqual(expected, result)
 
 
-    
+
 # Run the tests
 if __name__ == '__main__':
     unittest.main()
